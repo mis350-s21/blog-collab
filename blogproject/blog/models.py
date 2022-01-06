35b9e52597273579a1d6f4eea1ad8e829ea285f7
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2022-01-06 13:06:13 +0300

    Update models.py

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+    STATUS = (
+        (0, "Unpublished"),
+        (1, "Published"),
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100) #, unique=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title}: created at {self.created_at}"
+
+class Comment(models.Model):
+    comment = models.TextField()
+    author = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
