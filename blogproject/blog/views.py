21791e8b7cc31cd0725ef03848efe854f65e35d4
FatemaAlFailakawi <fatema.alfailakawi@cba.ku.edu.kw>
2022-01-06 13:35:52 +0300

    Detailed post view

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from blogproject.blog.models import Post
+
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    c = {'post_list': posts,}
+    return render(request,'post_list.html', c)
