d5d6321a09081d049ca6c0513496b5ccd4d6eb24
hajerayed9 <hajer.salman@cba.ku.edu.kw>
2022-01-06 13:36:05 +0300

    list pub post view

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from blogproject.blog.models import Post
+
 # Create your views here.
+
+
+def post_pub_list(request):
+  post = Post.objects.filter(status=1)
+  data = {
+    'pub_post': post,
+  }
+  return render(request, "post_list.html", data)
+
